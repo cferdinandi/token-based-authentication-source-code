bb895591c04a204b07fdc2437de98fe909b30a62
cferdinandi <chris@gomakethings.com>
2021-09-14 00:21:37 -0400

    Initial commit

---
+++ 01 - what is token-based authentication/01 - session.js
@@ -0,0 +1,16 @@
+//
+// An oversimplified version of session-based auth
+//
+
+let sessions = {
+	kfda987908123jlk: {
+		username: 'merlin@wizardschool.com',
+		display: 'Merlin',
+		permissions: ['admin', 'registration', 'homework']
+	},
+	rew7812iouio3frx: {
+		username: 'neville@wizardschool.com',
+		display: 'Neville',
+		permissions: ['registration', 'homework']
+	}
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# The Token-Based Authentication Pocket Guide Source Code
+All of the source code for the [Token-Based Authentication Pocket Guide](https://vanillajsguides.com).
